4eb7385882f37515d191f331835ecba2ec0fa852
Vitaliy Lyalin <vitaliy.lyalin@gmail.com>
2023-06-05 22:42:37 +0300

    Adds files contacts.json, contacts.js with using modules fs, pass and methods  fs.readFile, fs.writeFile, install Commander for command-line interfaces

---
+++ index.js
@@ -0,0 +1,40 @@
+const contacts = require('./contacts');
+const { program } = require('commander');
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case 'list':
+      const allContacts = await contacts.listContacts();
+      return console.table(allContacts);
+    case 'get':
+      const oneContact = await contacts.getContactById(id);
+      return console.log(oneContact);
+    case 'remove':
+      const deleteContact = await contacts.removeContact(id);
+      return console.log(deleteContact);
+    case 'add':
+      const newContact = await contacts.addContact({ name, email, phone });
+      return console.log(newContact);
+    case 'updateById':
+      const updateContact = await contacts.updateContact(id, {
+        name,
+        email,
+        phone,
+      });
+      return console.table(updateContact);
+    default:
+      console.warn('\x1B[31m Unknown action type!');
+  }
+};
+
+program
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
+
+program.parse(process.argv);
+
+const argv = program.opts();
+invokeAction(argv);
